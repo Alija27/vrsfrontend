fee0413899df3547228a0b554c2b95f407188cdd
Alija Ghimire <alijaghimire27@gmail.com>
2022-02-21 09:12:56 +0545

    UserCreate component created

--- src/Admin.js
+++ src/Admin.js
@@ -4,6 +4,8 @@ import { Sidebar } from './components/admin/layouts/Sidebar';
 import { Footer } from './components/admin/layouts/Footer';
 import { Dashboard } from './components/admin/layouts/Dashboard';
 import { Routes,Route } from 'react-router-dom';
+import { UserIndex } from './components/admin/users/UserIndex';
+import { UserCreate } from './components/admin/users/UserCreate';
 
 export const Admin = () => {
   return (
@@ -12,7 +14,8 @@ export const Admin = () => {
       <Sidebar/>
         <Routes>
            <Route path="/admin" element={<Dashboard/>}/>
-           
+           <Route path="/users" element={<UserIndex/>}/>
+           <Route path="/users/create" element={<UserCreate/>}/>
            
         </Routes>
        
---
+++ src/components/admin/users/UserCreate.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const UserCreate = () => {
+  return (
+    <div>
+
+    <div>
+        <div className="content-wrapper">
+        <div className="content-header">
+    <div className="container-fluid">
+      <div className="card m-1">
+        <div className="card-header">
+          <h3 className="card-title text-bold">Add New User</h3>
+          <div className="card-tools">
+            <Link to="/admin/users" className="btn-link btn-sm bg-indigo">
+             
+              <span>Go Back</span>
+            </Link>
+          </div>
+        </div>
+        <div className="card-body">
+        <form method="post" >
+            
+            <div className="form-group">
+              <label htmlFor="name">Name <span className="text-danger" title="Required">*</span></label>
+              <input type="text" name="name" id="name"  className="form-control " autofocus />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email<span className="text-danger" title="Required">*</span></label>
+              <input type="text" name="email" id="email"  className="form-control " />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Contact<span className="text-danger" title="Required">*</span></label>
+              <input type="text" name="phone" id="phone"  className="form-control " />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password<span className="text-danger" title="Required">*</span></label>
+              <input type="text" name="password" id="password" className="form-control " />
+            </div>
+            <div className="form-group">
+              <label htmlFor="document">Document</label>
+              <input type="file" name="document" id="document" className="form-control " />
+            </div>
+            <div className="form-group">
+              <label htmlFor="document">Role</label>
+              <select className="form-select">
+  <option className="Disabled">Select Role</option>
+  <option className="Disabled"value="admin">Admin</option>
+  <option className="Disabled"value="vendor">Vendor</option>
+  <option className="Disabled"value="customer">Customer</option>
+</select>
+            </div>
+            </form>
+        
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+    </div>
+ 
+
+    </div>
+  )
+}
