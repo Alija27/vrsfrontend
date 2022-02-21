53d9176639b3613a61c0ea48193eb4ccbc63d96b
Alija Ghimire <alijaghimire27@gmail.com>
2022-02-21 06:18:45 +0545

    Footer component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import { Header } from './components/admin/layouts/Header';
 import { Sidebar } from './components/admin/layouts/Sidebar';
+import { Footer } from './components/admin/layouts/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Sidebar/>
+      <Footer/>
     </div>
   );
 }
