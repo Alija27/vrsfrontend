57d4062a6b7099a98bdae376ba950ace69953743
Alija Ghimire <alijaghimire27@gmail.com>
2022-02-21 06:24:26 +0545

    Dashboard component created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Header } from './components/admin/layouts/Header';
 import { Sidebar } from './components/admin/layouts/Sidebar';
 import { Footer } from './components/admin/layouts/Footer';
+import { Dashboard } from './components/admin/layouts/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Sidebar/>
+      <Dashboard/>
       <Footer/>
     </div>
   );
